0b1a5e80b9b91658afa87bf4b3d9b8a5d861a648
Ian Kleinfeld <iankleinfeld@ians-mac-mini.lan>
2024-01-14 16:32:20 -0500

    ga-changed related_name to cars in carsapp.models

--- carsapp/serializers.py
+++ carsapp/serializers.py
@@ -1,9 +1,11 @@
 # carsapp/serializers.py
 
 from rest_framework import serializers
-from .models import Car
+from .models import Car, User
 
-class CarSerializer(serializers.ModelSerializer):
+class CarSerializer(serializers.HyperlinkedModelSerializer):
+    
+    
     class Meta:
         model = Car
         fields = '__all__'
